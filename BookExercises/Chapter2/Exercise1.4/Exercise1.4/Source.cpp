8c86a207f033158350f80687b301f95c8fffc09c
Alsayed Aldkhakhni <Alsayed Ali>
2024-01-30 22:40:34 +0200

    A small and easy program that print a poem by 'Ogden Nash'

---
+++ BookExercises/Chapter2/Exercise1.4/Exercise1.4/Source.cpp
@@ -0,0 +1,21 @@
+/*
+ * Subject: A small and easy program that print a poem by 'Ogden Nash'. 
+ *
+ * Date: 10 / 11 / 2023...... 11th from October.
+ *
+ * Written by: Alsayed Ali.
+ */
+
+#include<iostream>   					// That 'header file' essential for using 'cin' and 'cout'.
+using namespace std; 					// That namespace, container, contains the actual definition of 'cin' & 'cout'.
+
+int main()
+{
+
+	cout << "Candy is dandy,\n"			// Print tha first part of that poem and start from a next line.
+		 << "But liquor is quicker.";   // The second part of the poem printed in the next line.
+
+	cout << "\n---------------------";	// This line to make the output more clearer.
+
+	return 0;							// flags the 'OS' that the program executed successfully.
+}
