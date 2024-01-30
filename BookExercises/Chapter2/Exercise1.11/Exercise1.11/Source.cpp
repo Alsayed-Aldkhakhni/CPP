103c304b26d249eca2a976ea143116e840dc07c3
Alsayed Aldkhakhni <Alsayed Ali>
2024-01-30 23:03:52 +0200

    Print a table of some data using 'setw'

---
+++ BookExercises/Chapter2/Exercise1.11/Exercise1.11/Source.cpp
@@ -0,0 +1,55 @@
+/*
+ * Subject: A program to print some data about 3 persons in a table.
+ *
+ * Date: October 12, 2023.
+ *
+ * Written by: Alsayed Aldkhakhni.
+ */
+
+
+#include<iostream>						// 'iostream' is a header file that contains the declaration of 'cin' and 'cout'.
+#include<iomanip>						// 'iomanip' is, guess what :), also a header file essential for 'setw' and 'setiosflags'.
+using namespace std;					// This namespace 'standard' contains the definition of 'cin' and 'cout'.
+
+int main()
+{
+										// print the headers of the table.
+	cout << setw(15) << setiosflags(ios::left) << "Last name" 
+		 << setw(15) << setiosflags(ios::left) << "First name" 
+		 << setw(20) << setiosflags(ios::left) << "Street address" 
+		 << setw(15) << setiosflags(ios::left) << "Town" 
+		 << setw(15) << setiosflags(ios::left) << "State";
+
+										// This line is used to separate between the headers and the data.
+	cout << "\n-----------------------------------------------------------------------\n";
+
+	
+										// first record.
+	cout << setw(15) << setiosflags(ios::left) << "Jones"
+		 << setw(15) << setiosflags(ios::left) << "Bernard"
+		 << setw(20) << setiosflags(ios::left) << "109 Pine Lane"
+		 << setw(15) << setiosflags(ios::left) << "Littletown"
+		 << setw(15) << setiosflags(ios::left) << "MI";
+
+	cout << '\n';
+										// second record.
+	cout << setw(15) << setiosflags(ios::left) << "O'Brain"
+		 << setw(15) << setiosflags(ios::left) << "Coleen"
+		 << setw(20) << setiosflags(ios::left) << "42 E. 99th Ave."
+		 << setw(15) << setiosflags(ios::left) << "BigCity"
+		 << setw(15) << setiosflags(ios::left) << "NY";
+
+	cout << '\n';
+
+										// third record.
+	cout << setw(15) << setiosflags(ios::left) << "Wong"
+		 << setw(15) << setiosflags(ios::left) << "Harry"
+		 << setw(20) << setiosflags(ios::left) << "121-A Alabama St."
+		 << setw(15) << setiosflags(ios::left) << "Lakeville"
+		 << setw(15) << setiosflags(ios::left) << "IL";
+
+	cout << '\n';
+
+
+	return 0;							 // flags the 'OS' that the program is done.
+}
