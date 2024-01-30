37407cdc484de8cc52df2f9da37c2f21f0070bc8
Alsayed Aldkhakhni <Alsayed Ali>
2024-01-30 23:07:35 +0200

    Convert decimal pounds from new notation to the old British monetary notation.

---
+++ BookExercises/Chapter2/Exercise1.12/Exercise1.12/Source.cpp
@@ -0,0 +1,33 @@
+/*
+ * Subject: convert decimal pounds to the old British monetary notation.
+ *
+ * Date: October 12, 2023
+ *
+ * Written by: Alsayed Aldkhakhni.
+*/
+
+#include<iostream>									// 'iostream' is a header file that contains the declaration of 'cin' and 'cout'.
+using namespace std;								// namespace 'standard' is essential for 'cin' and 'cout' which contains their definition.
+
+int main()
+{					
+	float decimalAmount = 0.0;						// To hold the amount entered by the user
+
+	cout << "Enter decimal pounds: ";				// asking the user to enter decimal amount in the new British monetary system.
+	cin >> decimalAmount;							// Get that amount.
+
+	int  pounds = static_cast<int>(decimalAmount);  // pounds.
+													// reset fraction converted to shillings.
+	float fracShilling = (decimalAmount - pounds) * 20; 
+
+	int shilling = static_cast<int>(fracShilling);  // the integer shillings 
+													//		and the rest fraction is to pence.
+	float fracPence = (fracShilling - shilling) * 12;
+
+	int pence = static_cast<int>(fracPence);		// the final pence and the reset of the fraction is not neccessary.
+
+													// For the final step, I will print the old notation in the converted form.
+	cout  << "\n\x9c" << pounds << '.' << shilling << '.' << pence << ".\n";
+
+	return 0;										// indicates the 'OS' that the program executed successfully.
+}
