2d981b78bb80e392298dc9d0b3fdf4313417cc2b
Alsayed-Aldkhakhni <alsayedaldkhakhni@gmail.com>
2024-07-02 09:13:34 +0100

    Print a quote.

---
+++ Book/BookExercises/Chapter02/Ex04/Source.cpp
@@ -0,0 +1,22 @@
+/*
+ * Subject: Print a poem by 'Ogden Nash'. 
+ *
+ * Date: October 11, 2023
+ *
+ * Written by: Alsayed Ali.
+ *
+ * Last modification: May 16, 2024
+ */
+
+#include<iostream>  // That 'header file' essential for using 'cin' and 'cout'.
+using namespace std;// That namespace, container, contains the actual definition of 'cin' & 'cout'.
+
+int main()
+{
+	// Print tha first part of that poem and start from a next line.
+	cout << "Candy is dandy,\n"	
+		 << "But liquor is quicker.\n";
+
+	// flags the 'OS' that the program executed successfully.
+	return 0;
+}
