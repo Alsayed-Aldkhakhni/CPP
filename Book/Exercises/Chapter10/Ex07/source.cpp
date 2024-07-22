db2d59fc71aa4ddd8af3ae4a7d9d641377f91090
Alsayed-Aldkhakhni <alsayedaldkhakhni@gmail.com>
2024-07-22 10:27:11 +0100

    Ordering an array of pointers of Person objects.

---
+++ Book/Exercises/Chapter10/Ex07/source.cpp
@@ -0,0 +1,108 @@
+/*
+ * Subject: Ordering an array of pointers of Person objects.
+ *
+ * Date: July 22, 2024
+ * 
+ * Author: Alsayed-Aldkhakhni
+ *
+ * Last modification: July 22, 2024
+ *
+ * */
+
+#include <iostream> // for the declaration of cin, cout objects.
+#include <cstring>  // for the library function strlen.
+using namespace std;// for their definition.
+//#################################################################//
+class Person
+{
+private:
+	string name;
+	float salary;
+
+public:
+	Person() : salary(0.0), name("") {cin.ignore(1);}
+
+	void setData()
+	{
+		cout << "   Enter Person's name:   ";
+		getline(cin, name);
+
+		cout << "   Enter Person's salary: ";
+		cin >> salary;
+
+		if(salary <= 0)
+		{
+			cout << "Invalid salaryensation.\n";
+			exit(1);
+		}
+	}
+
+	void getData() const 
+	{
+		cout << "   Person's name:    " << name << "\n"
+		     << "   Person's salary: $" << salary << "\n";
+	}
+
+	float getSalary() const { return salary;}
+};
+//#################################################################//
+
+// functions' prototypes.
+void sort(Person**, int);
+void display(Person**, int);
+
+int main()
+{
+	Person* pers[10];
+	int numOfPers = 0;
+	char ans;
+
+	for(int i = 0;; i++)
+	{
+		pers[i] = new Person;
+		pers[i]->setData();
+		numOfPers++;
+
+		cout << "\nOne more?(y, n): ";
+		cin >> ans;
+
+		if(ans != 'y') break;
+	}
+
+	sort(pers, numOfPers);
+	display(pers, numOfPers);
+
+	return 0;
+}
+
+
+// functions' bodies.
+// p1 p2 p3 p4
+// 0  1  2  3
+void sort(Person** persPtr, int numOfPers)
+{
+	for(int i = 0; i < numOfPers-1; i++)
+	{
+		for(int j = i+1; j < numOfPers; j++)
+		{
+			// Don't forget: -> takes precedece over *
+			if((*(persPtr+i))->getSalary() > (*(persPtr+j))->getSalary())
+			{
+				Person* temp = *(persPtr+i);
+				*(persPtr+i) = *(persPtr+j);
+				*(persPtr+j) =   temp;
+			}
+		}
+	}
+}
+
+
+void display(Person** persPtr, int numOfPers)
+{
+	for(int i = 0; i < numOfPers; i++)
+	{
+		(*(persPtr+i))->getData();
+		cout << "=============================\n";
+	}
+}
+
