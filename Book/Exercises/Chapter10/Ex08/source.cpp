550369f84514f2539a19797a60cb920cee9f4632
Alsayed-Aldkhakhni <alsayedaldkhakhni@gmail.com>
2024-07-22 10:43:04 +0100

    Add items to the linked list at the back[tail].

---
+++ Book/Exercises/Chapter10/Ex08/source.cpp
@@ -0,0 +1,102 @@
+/*
+ * Subject: Linked list implementation.
+ *
+ * Date: July 22, 2024
+ *
+ * Author: Alsayed-Aldkhakhni
+ *
+ * Last modification: July 22, 2024
+ *
+ * */
+
+#include <iostream> // for the declaration of cin, cout objects.
+using namespace std;// for their definition.
+
+//########################################################//
+struct Link
+{
+	int data;
+	Link* next;
+};
+
+class LinkedList
+{
+private:
+	Link* first;
+	Link* last;
+
+public:
+	LinkedList(): first(NULL), last(NULL) {}
+	void addItem(int d);
+	void display();
+	~LinkedList();
+};
+
+// add new items.
+void LinkedList::addItem(int d)
+{
+	Link* newLink = new Link;
+	newLink->data = d;
+
+	if(first == NULL)
+		first = last = newLink;
+	else
+	{
+		last->next = newLink;
+		last = newLink;
+	}
+}
+
+// display the list's content.
+void LinkedList::display()
+{
+	Link* current = first;
+	while( current != NULL )
+	{
+		cout << current->data << endl;
+		current = current->next;
+	}
+}
+
+// destroy the list.
+LinkedList::~LinkedList()
+{
+	Link* temp = first;
+
+	while(first != NULL)
+	{
+		first = first->next;
+
+		temp->next = NULL;
+		//cout << temp->data << " is destroyed.\n";
+		delete temp;
+
+		temp = first;
+	}
+}
+//########################################################//
+
+int main()
+{
+	LinkedList lst1;
+
+	// add some items.
+	lst1.addItem(12);
+	lst1.addItem(13);
+	lst1.addItem(14);
+	lst1.addItem(15);
+	lst1.addItem(16);
+	lst1.addItem(17);
+	lst1.addItem(18);
+	lst1.addItem(19);
+	lst1.addItem(20);
+	lst1.addItem(21);
+
+	// display the items.
+	lst1.display();
+
+	// indicates a successful execution, popping the function off the stack.
+	return 0;
+}
+
+
