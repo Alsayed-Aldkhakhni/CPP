79f229fae4c2f5ba99e698ebfc0f1c0412cdaa3c
Alsayed Ali <alsayedaldkhakhni@gmail.com>
2024-08-25 13:59:59 +0300

    Test the tree.

--- DataStructures/BinarySearchTree/source.cpp
+++ DataStructures/BinarySearchTree/source.cpp
@@ -2,18 +2,18 @@
 // Name        : BinarySearchTree.cpp
 // Date	       : July 13, 2024
 // Author      : Alsayed-Aldkhakhni
-// Version     : V1.0
 // Copyright   : Nothing to be mentioned yet.
 // Description : Create our own tree data structure.
 //============================================================================
-#include <iostream> // for cin, cout declaration.
-using namespace std;// for their definition.
+
 #include "BinarySearchTree.h" // reference to the 'BinaaySearchTree' class.
+#include <iostream>           // for cin, cout declaration.
+using namespace std;          // for the definition of cin, cout.
 
 int main()
 {
 	// Instantiate an object of our homemade binary tree.
-	BinarySearchTree tree1;
+	BinarySearchTree<int> tree1;
 
 	try
 	{
@@ -22,7 +22,7 @@ int main()
 		tree1.insert(15);
 		tree1.insert(79);
 		tree1.insert(90);
-
+		
 		tree1.insert(10);
 		tree1.insert(55);
 		tree1.insert(12);
@@ -31,7 +31,7 @@ int main()
 
 		// show the data in what manner.
 		short ans;
-		cout << "Choose display method...\n\t[1.pre, 2.post, 3.in]: ";
+		cout << "Choose the digit.\n[1.pre, 2.post, 3.in]: ";
 		cin >> ans;
 
 		// display the data in one of the 3 ways we made.
@@ -39,15 +39,13 @@ int main()
 		{
 		case 1:
 			tree1.displayPreOrder(); break;
-
 		case 2:
 			tree1.displayPostOrder(); break;
-
 		case 3:
 			tree1.displayInOrder(); break;
-
 		default:
 			cout << "Not available.\n";
+			exit(1);
 		}
 
 		// search for an item in the tree.
@@ -58,19 +56,26 @@ int main()
 
 		// get the minimum of maximum number in the tree.
 		char minMax;
-		cout << "Search for the minimum or the maximum value(-, +): ";
+		cout << "Find min/max, hit a sign of(-, +): ";
 		cin >> minMax;
 
-		if(minMax == '-')
+		switch(minMax)
+		{
+		case '-':
+			cout << "The minimum item in the tree: ";
 			cout << tree1.findMin() << "\n\n";
-		else if(minMax == '+')
-				cout << tree1.findMax() << "\n\n";
-		else
+			break;
+		case '+':
+			cout << "The maxmum item in the tree: ";
+			cout << tree1.findMax() << "\n\n";
+			break;
+		default:
 			cout << "Invalid input.\n";
-
+			exit(1);
+		}
+		
 		// delete a node from the tree.
 		int delNode;
-
 		cout << "Enter a node to be deleted: ";
 		cin >> delNode;
 
@@ -81,20 +86,22 @@ int main()
 		{
 		case 1:
 			tree1.displayPreOrder(); break;
-
 		case 2:
 			tree1.displayPostOrder(); break;
-
 		case 3:
 			tree1.displayInOrder(); break;
-
 		default:
 			cout << "Not available.\n";
 		}
 
-	}catch(BinarySearchTree::Exception&)
+	}catch(const BinarySearchTree<int>::Exception&)
 	{}
 	
 	// indicates a successful execution.
 	return 0;
 }
+
+
+
+
+
