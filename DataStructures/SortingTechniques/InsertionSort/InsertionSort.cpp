953feea76dc806a5808ed7709a1ae5483ae67644
Alsayed Ali <alsayedaldkhakhni@gmail.com>
2024-08-28 10:14:42 +0300

    Sorting two arrays using Insertion sort algorithm.

--- DataStructures/SortingTechniques/InsertionSort/InsertionSort.cpp
+++ DataStructures/SortingTechniques/InsertionSort/InsertionSort.cpp
@@ -6,53 +6,21 @@
 // Description : Insertion sort technique implementation.
 // Complexity  : Time: O(n^2), Space: O(1) since it is an in-place technique.
 //============================================================================
-
 #include <iostream> // for cin, cout objects declaration.
 using namespace std;// for their definition.
 
-// functions' prototype.
-template<class Type>
-void InsertionSort(Type*, int);
-
-template<class Type>
-void display(Type*, int);
-
-int main()
-{
-	int intArr[] = {13, -10, 12, -100, 150, 110, 55, -210, 7, 8};
-	char charArr[] = {'x', 'A', 'C', 'm', 'R', 'n', 'j', 'W', 'u', 'P'};
-
-	// dynamic calculation to the size of the arrays.
-	int intSize = sizeof(intArr) / sizeof(intArr[1]);
-	int charSize = sizeof(charArr)/ sizeof(charArr[1]);
-
-	// array of integers.
-	cout << "Before:  "; display(intArr, intSize);
-	InsertionSort(intArr, intSize);
-	cout << "\nAfter: "; display(intArr, intSize);
-
-	cout << "\n\n******************************\n\n";
-
-	// array of characters.
-	cout << "Before: "; display(charArr, charSize);
-	InsertionSort(charArr, charSize);
-	cout << "\nAfter: "; display(charArr, charSize);
-
-	// indicates a successful execution.
-	return 0;
-}
-
-// functions' definition.
-template<class Type>
-void InsertionSort(Type* arr, int len)
+// the sort method.
+template<class T>
+void sort(T* arr, int len)
 {
 	int key, j;
-	for(int i = 1; i < len; i++)    // find the current item to be sorted.
+	for(int i = 1; i < len; i++)
 	{
 		key = arr[i];
 		j = i - 1;
 
-		while(j >= 0 && arr[j] > key)// shifting part.
+		// shifting part.
+		while(j >= 0 && arr[j] > key)
 		{
 			arr[j+1] = arr[j];
 			j = j-1;
@@ -62,12 +30,43 @@ void InsertionSort(Type* arr, int len)
 	}
 }
 
-template<class Type>
-void display(Type* arr, int size)
+// display the array.
+template<class T>
+void display(T* arr, int size)
 {
-	cout << "\n============================================================================\n|";
+	cout << "\n\n|";
 	for(int j = 0; j < size; j++)
 		cout << "  " << arr[j] << "  |";
-	cout << "\n============================================================================\n";
-
+	cout << "\n\n";
 }
+
+
+
+int main()
+{
+	int intArr[] = {13, -10, 12, -100, 150};
+	char charArr[] = {'x', 'A', 'C', 'm', 'R'};
+
+	// dynamic calculation to the size of the arrays.
+	int intSize = sizeof(intArr) / sizeof(intArr[1]);
+	int charSize = sizeof(charArr)/ sizeof(charArr[1]);
+
+	// array of integers.
+	cout << "Before:  "; display(intArr, intSize);
+	sort(intArr, intSize);
+	cout << "\nAfter: "; display(intArr, intSize);
+
+	cout << "\n\n";
+
+	// array of characters.
+	cout << "Before: "; display(charArr, charSize);
+	sort(charArr, charSize);
+	cout << "\nAfter: "; display(charArr, charSize);
+
+	// indicates a successful execution.
+	return 0;
+}
+
+
+
+
